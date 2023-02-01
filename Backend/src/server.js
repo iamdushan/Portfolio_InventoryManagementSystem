2200389ec6cd1fddd5d215156e26ff86cd516080
iamdushan <iamdushan95@gmail.com>
2023-02-01 08:12:17 +0530

    Commited

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,1 +1,13 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express(); //assign the express() to app variable to create express() app
+const PORT = process.env.PORT || "8090";
+
+app.use(cors()); //use the cors() package (to restrict 3rd party req)
+app.use(express.json({ limit : "20mb" })); //to customize req body data
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on PORT ${PORT}`);
+}); //to start express() app
